3f709a9e6a4e9f60d8c9b584423cbdced92d6fd4
sundylgz <1159279222@qq.com>
2019-03-05 14:46:13 +0800

    Initial commit

---
+++ src/main/java/com/sundy/service/HelloService.java
@@ -0,0 +1,14 @@
+package com.sundy.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+
+}
+
